415e7532484578e218712e4ece8f9f55100117cd
Badbroken <szdhasan71@gmail.com>
2020-09-21 19:21:03 +0600

    Building single file componenerts

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    @{{ user.username }} - {{fullName}}
+    <strong>Followers: </strong> {{followers}}
+    <button @click="followUser">Follow</button>
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return{
+      followers: 0,
+      user: {
+        username: 'vernon',
+        firstName: 'Vernon',
+        lastName: 'here',
+        email: 'shajidvai@gmail.com',
+        isAdmin: true
+      }
+    }
+  },
+  computed: {
+    fullName(){
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount){
+      if (oldFollowerCount < newFollowerCount){
+        console.log(`${this.user.username} has gained a follower`)
+      }
+    }
+  },
+  methods: {
+    followUser(){
+      this.followers++
+
+    }
+  },
+  mounted() {
+    this.followUser()
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+}
+</style>
